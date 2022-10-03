870a98017855c8112d6920d6207cbe4926638a69
Iminguet <i.minguet@hotmail.com>
2022-10-03 17:22:16 +0200

    iniciada función que cambiará entre los divs

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const goSection = (userClicked) => {
+  const displayOn = document.getElementById(userClicked);
+  const displayOff = document.getElementsByClassName('size');
+
+  // for (let i = 0; i < displayOff.length; i++) {
+  //   if (displayOff[i].id == displayOn.id) {
+  //     console.log(`has clickado en ${displayOn.id}`);
+  //   }
+  //   console.log(displayOff[i]);
+  // }
+};
