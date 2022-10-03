85e64ac470aa354b5ab6a3668678a7d1719a47ba
Iminguet <i.minguet@hotmail.com>
2022-10-04 01:04:16 +0200

    Implementada la funcion que switchea los divs

--- js/main.js
+++ js/main.js
@@ -2,10 +2,8 @@ const goSection = (userClicked) => {
   const displayOn = document.getElementById(userClicked);
   const displayOff = document.getElementsByClassName('size');
 
-  // for (let i = 0; i < displayOff.length; i++) {
-  //   if (displayOff[i].id == displayOn.id) {
-  //     console.log(`has clickado en ${displayOn.id}`);
-  //   }
-  //   console.log(displayOff[i]);
-  // }
+  for (let i = 0; i < displayOff.length; i++) {
+    displayOff[i].classList.add('visibilityOFF');
+  }
+  displayOn.classList.remove('visibilityOFF');
 };
