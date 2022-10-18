8132a8e58c50799edd24398092ac68a835406c69
Iminguet <i.minguet@hotmail.com>
2022-10-18 23:53:52 +0200

    Se ha creado el constructor de la clase heroe HTML, añadidos los onclick que pasan el id del elemento con la imagen del avatar

--- js/main.js
+++ js/main.js
@@ -16,14 +16,37 @@ y los use para crear la clase héroe.
 Si una de las variables es undefined, mensaje de error al clickar start
 */
 
+const startGame = (id) => {
+  goSection(id);
+  const name = document.getElementById('warriorId').value;
+  return name;
+};
+
+const getAvatar = (id) => {
+  const avatar = document.getElementById(id).innerHTML;
+  showActions.innerHTML = avatar;
+};
+
+const getName = () => {
+  const name = document.getElementById('warriorId').value;
+  console.log(name);
+};
+
 class Hero {
-  constructor(avatar, name, perk) {
+  constructor(avatar, name) {
     (this.avatar = avatar),
       (this.name = name),
-      (this.perk = perk),
       (this.minimunDamage = 0),
       (this.armour = 0),
       (this.life = 100);
+  }
+
+  addLife() {
+    this.life += 20;
+  }
+
+  addArmour() {
+    this.armour += 30;
   }
 
   ligthAttack(enemy) {
@@ -44,29 +67,31 @@ class Hero {
         this.minimunDamage
     );
   }
-
-  defensa() {}
 }
 
-const playerLife = document.getElementById('playerLife');
-const playerArmour = document.getElementById('playerArmour');
-const playerName = document.getElementById('playerName');
-const playerDamage = document.getElementById('playerDamage');
-const showActions = document.getElementById('showActions');
-const barbarian = document.getElementById('barbarian').innerHTML;
+let hero = new Hero(getAvatar, getName);
+
+const salud = () => hero.addLife();
+const armadura = () => hero.addArmour();
+
+const playerSelection = armadura;
+const heroe = playerSelection;
+heroe();
+
+// const playerLife = document.getElementById('playerLife');
+// const playerArmour = document.getElementById('playerArmour');
+// const playerName = document.getElementById('playerName');
+// const playerDamage = document.getElementById('playerDamage');
+// const showActions = document.getElementById('showActions');
+// const barbarian = document.getElementById('barbarian').innerHTML;
 
 // showActions.innerHTML = barbarian;
-console.log(barbarian);
+// console.log(barbarian);
 
-let hero = new Hero(barbarian, 'Antofagasto EL grande', [
-  'perk',
-  Math.random(),
-]);
-
-showActions.innerHTML = hero.avatar;
-playerName.innerHTML = hero.name;
-playerDamage.innerHTML = hero.minimunDamage;
-playerArmour.innerHTML = hero.armour;
+// showActions.innerHTML = hero.avatar;
+// playerName.innerHTML = hero.name;
+// playerDamage.innerHTML = hero.minimunDamage;
+// playerArmour.innerHTML = hero.armour;
 
 function restarSalud() {
   hero.life -= 10;
@@ -75,8 +100,15 @@ function restarSalud() {
   monsterLife.style.width = heroSalud;
 }
 
-setTimeout(restarSalud, 3000);
-setTimeout(restarSalud, 6000);
-setTimeout(restarSalud, 9000);
-setTimeout(restarSalud, 12000);
-setTimeout(restarSalud, 15000);
+// setTimeout(restarSalud, 3000);
+// setTimeout(restarSalud, 6000);
+// setTimeout(restarSalud, 9000);
+// setTimeout(restarSalud, 12000);
+// setTimeout(restarSalud, 15000);
+
+console.log(`aqui la salud del héroe ${hero.life}`);
+console.log(`aqui la armadura del héroe ${hero.armour}`);
+
+// Y SI LO ARREGLAMOS CON HERENCIA??
+
+// https://es.javascript.info/class-inheritance
