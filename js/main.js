1a6692b2fc3c87fabdf2c4d78d1c91839d06a2da
Iminguet <i.minguet@hotmail.com>
2022-10-20 23:52:21 +0200

    Se ha creado la clase principal "Character". Se ha dibujado la función que recoge el avatar que seleccione el usuario. Se ha dibujado la función que tome el nombre Se ha limpiado un poco el código principal.

--- js/main.js
+++ js/main.js
@@ -17,22 +17,23 @@ Si una de las variables es undefined, mensaje de error al clickar start
 */
 
 const startGame = (id) => {
+  // validar los datos
+  // Nombre ?
+  // Avatar?
+  // Skill?
+  // Crear clase --> Pasar a pantalla de juego.
+
   goSection(id);
-  const name = document.getElementById('warriorId').value;
-  return name;
 };
 
-const getAvatar = (id) => {
-  const avatar = document.getElementById(id).innerHTML;
-  showActions.innerHTML = avatar;
-};
+// recoge el avatar que seleccione el usuario
+const getAvatar = (id) => document.getElementById(id).innerHTML;
 
-const getName = () => {
-  const name = document.getElementById('warriorId').value;
-  console.log(name);
-};
+// recoge el nombre del input
+const getName = () => document.getElementById('warriorId').value;
 
-class Hero {
+// Clase principal character, con esta clase se construirá el héroe y los monstruos
+class Characters {
   constructor(avatar, name) {
     (this.avatar = avatar),
       (this.name = name),
@@ -41,74 +42,29 @@ class Hero {
       (this.life = 100);
   }
 
-  addLife() {
-    this.life += 20;
-  }
-
-  addArmour() {
-    this.armour += 30;
-  }
-
   ligthAttack(enemy) {
-    enemy.life -= parseInt(
-      Math.random() * (this.minimunDamage + 10 - this.minimunDamage) +
-        this.minimunDamage
-    );
+    enemy.life -=
+      parseInt(
+        Math.random() * (this.minimunDamage + 10 - this.minimunDamage) +
+          this.minimunDamage
+      ) - enemy.armour;
   }
   normalAttack(enemy) {
-    enemy.life -= parseInt(
-      Math.random() * (this.minimunDamage + 15 - this.minimunDamage) +
-        this.minimunDamage
-    );
+    enemy.life -=
+      parseInt(
+        Math.random() * (this.minimunDamage + 15 - this.minimunDamage) +
+          this.minimunDamage
+      ) - enemy.armour;
   }
   strongAttack(enemy) {
-    enemy.life -= parseInt(
-      Math.random() * (this.minimunDamage + 25 - this.minimunDamage) +
-        this.minimunDamage
-    );
+    enemy.life -=
+      parseInt(
+        Math.random() * (this.minimunDamage + 25 - this.minimunDamage) +
+          this.minimunDamage
+      ) - enemy.armour;
   }
 }
 
-let hero = new Hero(getAvatar, getName);
+let hero = new Characters(getAvatar, getName);
 
-const salud = () => hero.addLife();
-const armadura = () => hero.addArmour();
-
-const playerSelection = armadura;
-const heroe = playerSelection;
-heroe();
-
-// const playerLife = document.getElementById('playerLife');
-// const playerArmour = document.getElementById('playerArmour');
-// const playerName = document.getElementById('playerName');
-// const playerDamage = document.getElementById('playerDamage');
-// const showActions = document.getElementById('showActions');
-// const barbarian = document.getElementById('barbarian').innerHTML;
-
-// showActions.innerHTML = barbarian;
-// console.log(barbarian);
-
-// showActions.innerHTML = hero.avatar;
-// playerName.innerHTML = hero.name;
-// playerDamage.innerHTML = hero.minimunDamage;
-// playerArmour.innerHTML = hero.armour;
-
-function restarSalud() {
-  hero.life -= 10;
-  let heroSalud = `${hero.life}%`;
-  playerLife.style.width = heroSalud;
-  monsterLife.style.width = heroSalud;
-}
-
-// setTimeout(restarSalud, 3000);
-// setTimeout(restarSalud, 6000);
-// setTimeout(restarSalud, 9000);
-// setTimeout(restarSalud, 12000);
-// setTimeout(restarSalud, 15000);
-
-console.log(`aqui la salud del héroe ${hero.life}`);
-console.log(`aqui la armadura del héroe ${hero.armour}`);
-
-// Y SI LO ARREGLAMOS CON HERENCIA??
-
-// https://es.javascript.info/class-inheritance
+let monster = new Characters(monsterAvatar, monsterName);
