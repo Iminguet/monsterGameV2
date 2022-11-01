e47fc236669889333a70367ba0dbf166fef8c8e4
Iminguet <i.minguet@hotmail.com>
2022-11-01 23:46:18 +0100

    Se ha creado el constructor de clases

--- js/main.js
+++ js/main.js
@@ -3,37 +3,12 @@
 let avatars = Array.from(document.getElementsByClassName('getAvatar'));
 let identifies = Array.from(document.getElementsByClassName('getId'));
 let buttons = Array.from(document.getElementsByClassName('toSection'));
+const showObj = document.getElementById('characterStartButton-mainGame');
 
 let playerSelections = {
-  name: '',
-  avatar: '',
-  skill: '',
-};
-
-// conseguimos el avatar
-const createAvatar = (avatarsArray) => {
-  const getAvatar = (e) => {
-    e.preventDefault();
-    playerSelections.avatar = e.currentTarget;
-    console.log(playerSelections.avatar);
-  };
-
-  for (const avatar of avatarsArray) {
-    avatar.addEventListener('click', getAvatar);
-  }
-};
-
-// Conseguimos el id
-const createId = (identifiesArray) => {
-  const getId = (e) => {
-    e.preventDefault();
-    playerSelections.skill = e.currentTarget.id;
-    console.log(playerSelections.skill);
-  };
-
-  for (const identity of identifiesArray) {
-    identity.addEventListener('click', getId);
-  }
+  name: undefined,
+  avatar: undefined,
+  skill: undefined,
 };
 
 // cambiamos la vista de las páginas
@@ -57,19 +32,136 @@ const changeView = (buttonsArray) => {
   }
 };
 
+// conseguimos el avatar del personaje
+const createAvatar = (avatarsArray) => {
+  const getAvatar = (e) => {
+    e.preventDefault();
+    playerSelections.avatar = e.currentTarget;
+    console.log(playerSelections.avatar);
+  };
+
+  for (const avatar of avatarsArray) {
+    avatar.addEventListener('click', getAvatar);
+  }
+};
+
+// conseguimos el nombre del personaje
+const getName = () => {
+  const playerName = document.getElementById('warriorId').value;
+  if (playerName.trim() === '') {
+    return 'Aureliano Buendía';
+  } else {
+    return playerName.trim();
+  }
+};
+
+// Conseguimos el id de la skill del personaje
+const createId = (identifiesArray) => {
+  const getId = (e) => {
+    e.preventDefault();
+    playerSelections.skill = e.currentTarget.id;
+    console.log(playerSelections.skill);
+  };
+
+  for (const identity of identifiesArray) {
+    identity.addEventListener('click', getId);
+  }
+};
+
 //Vamos a crear la clase
 
-const showObj = document.getElementById('muestraObjeto');
-const name = document.getElementById('warriorId').value;
+// acción del boton que lleva al principio del juego
 
-playerSelections.name = name;
+const canStart = () => {
+  showObj.addEventListener('click', (e) => {
+    e.preventDefault();
+    playerSelections.name = getName();
+    if (
+      playerSelections.name === undefined ||
+      playerSelections.avatar === undefined ||
+      playerSelections.skill === undefined
+    ) {
+      alert('Te as dejado algo por rellenar, mollano');
+    } else {
+      gameStart();
+    }
+  });
+};
 
-showObj.addEventListener('click', (e) => {
-  e.preventDefault();
+const gameStart = () => {
+  class Characters {
+    constructor(avatar, name) {
+      (this.avatar = avatar),
+        (this.name = name),
+        (this.potions = 2),
+        (this.minimunDamage = 0),
+        (this.armour = 0),
+        (this.life = 100);
+    }
 
-  console.log(playerSelections);
-});
+    ligthAttack(enemy) {
+      enemy.life -=
+        parseInt(
+          Math.random() * (this.minimunDamage + 10 - this.minimunDamage) +
+            this.minimunDamage
+        ) - enemy.armour;
+    }
+    normalAttack(enemy) {
+      enemy.life -=
+        parseInt(
+          Math.random() * (this.minimunDamage + 15 - this.minimunDamage) +
+            this.minimunDamage
+        ) - enemy.armour;
+    }
+    strongAttack(enemy) {
+      enemy.life -=
+        parseInt(
+          Math.random() * (this.minimunDamage + 25 - this.minimunDamage) +
+            this.minimunDamage
+        ) - enemy.armour;
+    }
+  }
+
+  const skills = {
+    increaseAttack: class IncreaseAttack extends Characters {
+      increaseAttack() {
+        this.minimunDamage += 5;
+      }
+    },
+
+    increaseDefense: class IncreaseDefense extends Characters {
+      increaseDefense() {
+        this.armour += 5;
+      }
+    },
+
+    morePotions: class MorePotions extends Characters {
+      morePotions() {
+        this.potions += 1;
+      }
+    },
+
+    increaseHealth: class IncreaseHealth extends Characters {
+      increaseHealth() {
+        this.life += 50;
+      }
+    },
+  };
+
+  const classHerecy = playerSelections.skill;
+
+  const hero = new skills[classHerecy](
+    playerSelections.avatar,
+    playerSelections.name
+  );
+  hero[classHerecy]();
+
+  console.log('******************************************************');
+  console.log(hero);
+  console.log('******************************************************');
+};
 
 createAvatar(avatars);
 createId(identifies);
 changeView(buttons);
+canStart();
